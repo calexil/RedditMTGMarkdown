b54a8478e0c6cd81da84b741ca3e98f3b5bf8a24
JD <calexil@users.noreply.github.com>
2026-06-17 19:41:53 -0400

    Add files via upload

---
+++ deckproxy.php
@@ -0,0 +1,29 @@
+<?php
+
+$url = $_GET['url'] ?? '';
+
+if (!$url) {
+    http_response_code(400);
+    exit('Missing URL');
+}
+
+$ch = curl_init($url);
+
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_USERAGENT => 'Mozilla/5.0',
+    CURLOPT_TIMEOUT => 30,
+]);
+
+$html = curl_exec($ch);
+
+if ($html === false) {
+    http_response_code(500);
+    exit(curl_error($ch));
+}
+
+curl_close($ch);
+
+header('Content-Type: text/html; charset=utf-8');
+echo $html;
